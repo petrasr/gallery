b6abc9b415e219489a66e3c3e519c9ecc698ef62
rudolfpetras <rpetras@smithmicro.com>
2022-12-13 09:04:00 +0100

    second

---
+++ app/src/main/java/com/example/gallery/data/model/Tags.kt
@@ -0,0 +1,3 @@
+package com.example.gallery.data.model
+
+data class Tags(val tags: List<String>)
---
+++ app/src/main/java/com/example/gallery/utils/Status.kt
@@ -0,0 +1,7 @@
+package com.example.gallery.utils
+
+enum class Status {
+    SUCCESS,
+    ERROR,
+    LOADING
+}
